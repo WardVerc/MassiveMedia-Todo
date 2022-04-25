f67f6112ac19c9085cdf76ff1efa2dc73c336ea8
Ward Vercruyssen <ward.vercruyssen@hotmail.be>
2022-04-25 17:06:02 +0200

    Added getAllLists + createList

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, ScrollView, Text, View } from "react-native";
+import { styles } from "./App.styles";
+import CreateListDialog from "./components/Dialog/CreateListDialog";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import useGetAllLists from "./hooks/useGetAllLists";
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+  const { lists, isLoading, getLists } = useGetAllLists();
+
+  const handlePress = () => {
+    setVisible(true);
+  };
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Welcome, Massive Media!</Text>
       <StatusBar style="auto" />
+      <Header />
+      <CreateListDialog
+        visible={visible}
+        setVisible={setVisible}
+        getLists={getLists}
+      />
+      <ScrollView contentContainerStyle={styles.listsContainer}>
+        {lists.map((list) => {
+          return <List list={list} key={list.listId} />;
+        })}
+      </ScrollView>
+      <Button title="Add a list man" onPress={() => handlePress()} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ components/List/List.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    margin: 10,
+    width: "40%",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    elevation: 7,
+  },
+});
